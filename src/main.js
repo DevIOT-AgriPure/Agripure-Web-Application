795a66b7c942dbe5a8bf474f78a6b7a1f1a8d673
DiegoTalledoS18 <85322967+DiegoTalledoS18@users.noreply.github.com>
2023-09-25 01:23:06 -0500

    base app

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+//PrimeVue Styles
+import "primevue/resources/themes/arya-green/theme.css"       //theme
+import "primevue/resources/primevue.min.css"                //core css
+import "primeicons/primeicons.css"
+
+import Button from 'primevue/button';
+
+const app = createApp(App)
+
+app.component("pv-button",Button)
+
+app.mount('#app')
