ff63d5052bd42cbbc3dd79caa2b317b5f762997a
DiegoTalledoS18 <diegotasa35@hotmail.com>
2023-09-25 22:10:44 -0500

    feat(sidebar): fixing visibility

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -39,7 +39,7 @@
 <script setup>
 import { ref } from 'vue'
 import logoURL from '../assets/logo.png'
-const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
+const is_expanded = true
 const ToggleMenu = () => {
     is_expanded.value = !is_expanded.value
     localStorage.setItem("is_expanded", is_expanded.value)
