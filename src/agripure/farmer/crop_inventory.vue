b6dd837e3847d746f9ffe735429c67153540f29f
DiegoTalledoS18 <diegotasa35@hotmail.com>
2023-10-10 21:51:01 -0500

    feat(specialist): reset filter word

--- src/agripure/farmer/crop_inventory.vue
+++ src/agripure/farmer/crop_inventory.vue
@@ -203,9 +203,11 @@ export default {
     methods:{
         resetInventory(){
           this.currentInventoryResultsPlants=this.displayableCrops
+            this.searchInventorValue=""
         },
         resetAddPlant(){
             this.currentResultsPlants = this.defaultResultsPlants
+            this.searchNewPlantValue=""
         },
         inventorySearchSelected(){
             for (let i = 0; i < this.currentInventoryResultsPlants.length; i++) {
@@ -232,7 +234,7 @@ export default {
                 crop.name.toLowerCase().includes(this.searchInventorValue.toString().toLowerCase())
             );
             if(matchingCrops.length===0){
-                this.currentInventoryResultsPlants=this.displayableCrops
+                this.currentInventoryResultsPlants=[]
             }else {
                 this.searchInventorItems = matchingCrops.map(crop => crop.name);
                 this.currentInventoryResultsPlants=matchingCrops
@@ -246,7 +248,7 @@ export default {
                 this.newPlantsSearchOptions=response.data
                 let options=[]
                 if(response.data.length===0){
-                    this.currentResultsPlants=this.resultsPlants
+                    this.currentResultsPlants=[]
                 }else {
                     for (let i = 0; i < response.data.length; i++) {
                         options.push(response.data[i].name)
