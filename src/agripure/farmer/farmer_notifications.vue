575d3c8a6b04d7903d4f95bc0ce8a7a8d86e1b1d
DiegoTalledoS18 <diegotasa35@hotmail.com>
2023-10-19 17:21:29 -0500

    feat(devices): router navigation

--- src/agripure/farmer/farmer_notifications.vue
+++ src/agripure/farmer/farmer_notifications.vue
@@ -3,13 +3,19 @@
     <div style="margin: 2rem 2rem 2rem 2rem" >
       <div v-for="notification in notifications"
            :key="notification.id">
-        <div class="chat-card" @click="redirectTo()">
+        <div class="chat-card" @click="redirectTo(notification.notificationType)">
           <div class="profile-image">
-            <img :src="http" alt="Foto de perfil">
+            <img :src="notification.imageUrl" alt="Foto de perfil">
           </div>
           <div class="chat-content" >
             <div class="chat-header">
-              <h3 style="margin-bottom: 0.5rem">{{ notification.message }}</h3>
+                <div>
+                    <h3 style="margin-bottom: 0.5rem">{{ notification.message }}</h3>
+                    <p>{{this.calculateTime(notification.date)}}</p>
+                </div>
+                <div>
+                    <pv-button icon="pi pi-times" severity="danger" text rounded aria-label="Cancel"/>
+                </div>
             </div>
           </div>
         </div>
@@ -35,10 +41,69 @@ export default {
   created(){
     new NotificationService().getAllNotificationByUserId(1).then(response=>{
       this.notifications=response.data
+        const fecha = new Date(); // Obtiene la fecha y hora actual
+        this.getFormatDay(fecha)
+
     })
 
   },
   methods:{
+      redirectTo(notificationType){
+          console.log(notificationType)
+          switch (notificationType) {
+              case 'crop':
+                  console.log("crop");
+                  this.$router.push("/farmer/cropInventory")
+                  return 0
+
+              case 'request':
+                  console.log("request");
+                  this.$router.push("/farmer/specialist")
+                  return 0
+          }
+      },
+      getFormatDay(fecha){
+          // Formatea la fecha y la hora en una cadena legible
+          const fechaFormateada = `${fecha.toLocaleDateString()} ${fecha.toLocaleTimeString()}`;
+          //console.log(fechaFormateada);
+          const [fechaParte, horaParte] = fechaFormateada.split(' ');
+          // Divide la fecha en día, mes y año
+          const [dia, mes, año] = fechaParte.split('/');
+          // Divide la hora en horas, minutos y segundos
+          const [hora, minutos, segundos] = horaParte.split(':');
+          // Asegúrate de que JavaScript interprete el mes como 0-indexed (enero=0, febrero=1, etc.)
+          const fechaConvertida = new Date(Number(año), Number(mes) - 1, Number(dia), Number(hora), Number(minutos), Number(segundos));
+          //console.log(fechaConvertida);
+      },
+      calculateTime(dateTimeString) {
+          // Convierte la cadena en un objeto Date
+          const [fechaParte, horaParte] = dateTimeString.split(' ');
+          // Divide la fecha en día, mes y año
+          const [dia, mes, año] = fechaParte.split('/');
+          // Divide la hora en horas, minutos y segundos
+          const [hora, minutos, segundos] = horaParte.split(':');
+          // Asegúrate de que JavaScript interprete el mes como 0-indexed (enero=0, febrero=1, etc.)
+          const fechaHora = new Date(Number(año), Number(mes) - 1, Number(dia), Number(hora), Number(minutos), Number(segundos));
+
+          // Calcula la diferencia en milisegundos entre la fecha/hora proporcionada y la fecha/hora actual
+          const diferenciaEnMilisegundos = new Date() - fechaHora;
+
+          // Convierte la diferencia a segundos
+          const segundosTranscurridos = Math.floor(diferenciaEnMilisegundos / 1000);
+
+          if (segundosTranscurridos < 60) {
+              return "Hace un momento";
+          } else if (segundosTranscurridos < 3600) {
+              const minutos = Math.floor(segundosTranscurridos / 60);
+              return `Hace ${minutos} ${minutos === 1 ? "minuto" : "minutos"}`;
+          } else if (segundosTranscurridos < 86400) {
+              const horas = Math.floor(segundosTranscurridos / 3600);
+              return `Hace ${horas} ${horas === 1 ? "hora" : "horas"}`;
+          } else {
+              const dias = Math.floor(segundosTranscurridos / 86400);
+              return `Hace ${dias} ${dias === 1 ? "día" : "días"}`;
+          }
+      }
   }
 }
 </script>
