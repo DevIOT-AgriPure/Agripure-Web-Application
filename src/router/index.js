0bf8f37b8cfc93581b1701f2019234c09e4997cb
maucaillesamuel02 <diegotasa35@hotmail.com>
2023-10-24 12:29:26 -0500

    feat(sign-In): set default path

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
     routes: [
         {
             path: '/',
-            redirect: '/farmer/cropInventory',
+            redirect: '/sign-in',
         },
         {
             path: '/sign-in',
