46aa00d7a9af3076bd6b3ecfe074a672fba69afb
DiegoTalledoS18 <diegotasa35@hotmail.com>
2023-10-25 11:15:44 -0500

    feat(farmerNotification): added redirect button

--- src/agripure/farmer/farmer_notifications.vue
+++ src/agripure/farmer/farmer_notifications.vue
@@ -3,15 +3,18 @@
     <div style="margin: 2rem 2rem 2rem 2rem" >
       <div v-for="notification in notifications"
            :key="notification.id">
-        <div class="chat-card" @click="redirectTo(notification.notificationType)">
-          <div class="profile-image">
+        <div class="chat-card">
+          <div class="profile-image" @click="redirectTo(notification.notificationType)">
             <img :src="notification.imageUrl" alt="Foto de perfil">
           </div>
           <div class="chat-content" >
             <div class="chat-header">
-                <div>
+                <div @click="redirectTo(notification.notificationType)">
                     <h3 style="margin-bottom: 0.5rem">{{ notification.message }}</h3>
                     <p>{{this.calculateTime(notification.date)}}</p>
+                </div>
+                <div style="width: 100%" @click="redirectTo(notification.notificationType)">
+                    ‎
                 </div>
                 <div>
                     <pv-button icon="pi pi-times" severity="danger" text rounded aria-label="Cancel"/>
